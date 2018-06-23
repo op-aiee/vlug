0c4112eda320626aa2ad86b8d2cd1d6c6dd8d886
Sjors Ottjes <sjorsottjes@gmail.com>
2018-06-23 08:42:56 +0200

    add missing import

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Support\Carbon;
 use Spatie\Snapshots\MatchesSnapshots;
 use Illuminate\Contracts\Console\Kernel;
