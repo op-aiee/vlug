2be3ffac124cf62903fc6a2f1b6bf5065dd4ef68
Sjors Ottjes <sjorsottjes@gmail.com>
2018-10-19 11:35:15 +0200

    Update scripts.js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,6 +2,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
